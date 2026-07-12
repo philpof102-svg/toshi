6b13ba8c0d5b773dee5b15021409886210462d4d
Philippe <philpof98@gmail.com>
2026-07-12 14:56:38 +0200

    Revert "re-apply: multi-mascot catalog + wardrobe picker + motion-extend + skins (panel)"
    This reverts commit 62a4e38ed8f16d647ca49da3f43231b1f49b4c64.

--- test/probe-ls2.cjs
+++
@@ -1,8 +0,0 @@
-const localStorage = (() => {
-  const s = new Map();
-  return { getItem:(k)=>s.has(k)?s.get(k):null, setItem:(k,v)=>s.set(k,String(v)), removeItem:(k)=>s.delete(k), clear:()=>s.clear() };
-})();
-console.log('typeof:', typeof localStorage);
-console.log('typeof getItem:', typeof localStorage.getItem);
-localStorage.setItem('foo', 'bar');
-console.log('roundtrip:', localStorage.getItem('foo'));
