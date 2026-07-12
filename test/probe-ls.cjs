a37dc82de7a871ce561f83ef114e211d56a8a1c6
Philippe <philpof98@gmail.com>
2026-07-12 14:07:04 +0200

    Revert "feat(panel): multi-mascot catalog + wardrobe picker + motion-extend"
    This reverts commit b082b4b1a2ee63f5debf3fa4229db8c3b6788f3b.

--- test/probe-ls.cjs
+++
@@ -1,5 +0,0 @@
-console.log('typeof localStorage:', typeof localStorage);
-if (typeof localStorage !== 'undefined') {
-  console.log('keys:', Object.keys(localStorage).join(','));
-  console.log('typeof getItem:', typeof localStorage.getItem);
-}
